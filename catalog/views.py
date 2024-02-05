f2bf5e94ddfd9f6946e40c3c75c7995e457ef949
Nor sairi bin ahmad shahabudin <maxsim86@gmail.com>
2024-02-05 10:57:53 +0800

    catalog book

---
+++ catalog/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Book, Author, BookInstance, Genre, Language
+from django.views.generic import CreateView, DetailView
+
+
+# Create your views here.
+def index(request):
+    num_books = Book.objects.all().count
+    num_instance = BookInstance.objects.all().count()
+
+    num_instances_avail = BookInstance.objects.filter(status__exact="a").count()
+
+    context = {
+        "num_books": num_books,
+        "num_instance": num_instance,
+        "num_instances_avail": num_instances_avail,
+    }
+
+    return render(request, "catalog/index.html", context=context)
+
+
+class BookCreate(CreateView):  # book_form.html
+    model = Book
+    fields = "__all__"
+
+
+class BookDetail(DetailView):
+    model = Book
